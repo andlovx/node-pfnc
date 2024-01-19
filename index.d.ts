2c87a8efc6124b4f34bbd2d0a11621091de74235
Anders Lövgren <anders.lovgren@xertified.com>
2024-01-19 16:47:01 +0100

    Fixes package name and complement type documentation in typescript declarations.

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="node" />
 
-declare module "pfnc" {
+declare module "@xertified/pfnc" {
   /**
    * The origin of referred port.
    */
@@ -10,7 +10,13 @@ declare module "pfnc" {
    * Input option for scanner.
    */
   export type Options = {
+    /**
+     * The origin of referred port.
+     */
     origin: Origin;
+    /**
+     * The UDP/TCP network port to inquire connection information for.
+     */
     port: number;
   };
 
@@ -18,15 +24,30 @@ declare module "pfnc" {
    * Result array from scanner.
    */
   export type Result = {
+    /**
+     * The origin of port information.
+     */
     origin: string;
+    /**
+     * The other port of peer connection.
+     */
     port: number;
+    /**
+     * The process ID.
+     */
     pid: number;
+    /**
+     * The resolved absolute path of application utilizing the inquired port.
+     */
     path: string;
   }[];
 
   /**
-   * The scanner function.
+   * The scanner function. Call this function to get information on related
+   * peer network connection.
+   *
    * @param options Input options for scanning.
+   * @returns Array of result objects containing i.e. port, pid and program path.
    */
   export function scan(options: Options): Result;
 }
